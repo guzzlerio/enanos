7c4f076b247ba762456d958e3b7b8f4546bd4be5
REAANDREW <email@andrewrea.co.uk>
2015-01-22 13:10:09 +0000

    Enanos Server - Sneezy - Get returns random response body

--- enanos_test.go
+++ enanos_test.go
@@ -4,19 +4,45 @@ import (
 	"fmt"
 	"github.com/franela/goblin"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"testing"
 )
 
+type ResponseBodyGenerator interface {
+	Generate() string
+}
+
+type FakeResponseBodyGenerator struct {
+	use string
+}
+
+func (instance *FakeResponseBodyGenerator) Generate() string {
+	return instance.use
+}
+
+func (instance *FakeResponseBodyGenerator) Use(value string) {
+	instance.use = value
+}
+
+func NewFakeResponseBodyGenerator() *FakeResponseBodyGenerator {
+	return &FakeResponseBodyGenerator{""}
+}
+
 func Test_Enanos(t *testing.T) {
 	g := goblin.Goblin(t)
 	g.Describe("Enanos Server:", func() {
+
+		var fakeResponseBodyGenerator *FakeResponseBodyGenerator
+
 		url := func(path string) (fullPath string) {
 			fullPath = "http://localhost:8000" + path
 			return
 		}
 
 		g.BeforeEach(func() {
+			fakeResponseBodyGenerator = NewFakeResponseBodyGenerator()
+
 			go func() {
 				defaultHappy := func(w http.ResponseWriter, r *http.Request) {
 					w.WriteHeader(http.StatusOK)
@@ -26,6 +52,7 @@ func Test_Enanos(t *testing.T) {
 				}
 				defaultSneezy := func(w http.ResponseWriter, r *http.Request) {
 					w.WriteHeader(http.StatusOK)
+					w.Write([]byte(fakeResponseBodyGenerator.Generate()))
 				}
 				mux := http.NewServeMux()
 				mux.HandleFunc("/default/happy", defaultHappy)
@@ -56,6 +83,15 @@ func Test_Enanos(t *testing.T) {
 			g.It("GET returns 200", func() {
 				resp, _ := http.Get(url("/default/sneezy"))
 				assert.Equal(t, http.StatusOK, resp.StatusCode)
+			})
+
+			g.It("GET returns random response body", func() {
+				sample := "foobar"
+				fakeResponseBodyGenerator.Use(sample)
+				resp, _ := http.Get(url("/default/sneezy"))
+				defer resp.Body.Close()
+				body, _ := ioutil.ReadAll(resp.Body)
+				assert.Equal(t, sample, string(body))
 			})
 		})
 	})
