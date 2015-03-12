46dcdf1b7e28ddd89d91e5332eb4542c241d9b1d
Andrew Rea <email@andrewrea.co.uk>
2015-03-12 22:09:45 +0000

    Added more description to the application

--- main.go
+++ main.go
@@ -26,6 +26,17 @@ func responseCodeGeneratorFactory(codes []int) ResponseCodeGenerator {
 
 func main() {
 	kingpin.Version("0.0.1")
+	kingpin.CommandLine.Help = `
+	ENANOS
+
+	Is a HTTP server with several endpoints that can be used to substitute the actual http service dependencies of a system.  This tool allows developers to see how the system will perform against varying un-stable http services, each which exhibit different effects. Oh, and the names of the endpoints are the same as those of some well known disney characters:
+	Happy   - will return a 200 response code
+	Grumpy  - will return a random 5XX response code 
+	Sneezy  - will return a 200 response code but a response body with a size between <minSize> and <maxSize>
+	Sleepy  - will return a 200 response code but only after a random sleep between <minSleep> and <maxSleep>
+	Bashful - will return a random 3XX response code.  If the response code is one which redirects then Bashful will return its own location to invite an infinite redirect loop
+	Dopey   - will return a random 4XX response code
+	`
 	kingpin.Parse()
 	responseBodyGenerator := NewRandomResponseBodyGenerator(*minSize, *maxSize)
 	random := NewRealRandom()
