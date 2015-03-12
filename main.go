6cb9fb685ce4f4511117d9b6e370ac3f71438994
Andrew Rea <email@andrewrea.co.uk>
2015-03-12 21:42:08 +0000

    Added the min and max size of response in bytes as command line arguments

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"gopkg.in/alecthomas/kingpin.v1"
+	"strconv"
 	"time"
 )
 
@@ -15,6 +16,8 @@ var (
 	port     = kingpin.Flag("port", "the port to host the server on").Default("8000").Short('p').OverrideDefaultFromEnvar(ENV_ENANOS_PORT).Int()
 	minSleep = kingpin.Flag("min-sleep", "the minimum sleep time for sleepy in milliseconds").Default("1000").Int()
 	maxSleep = kingpin.Flag("max-sleep", "the maximum sleep time for sleepy in milliseconds").Default("60000").Int()
+	minSize  = kingpin.Flag("min-size", "the minimum size of response body for sneezy to generate").Default("1024").Int()
+	maxSize  = kingpin.Flag("max-size", "the maximum size of response body for sneezy to generate").Default(strconv.Itoa(1024 * 100)).Int()
 )
 
 func responseCodeGeneratorFactory(codes []int) ResponseCodeGenerator {
@@ -24,7 +27,7 @@ func responseCodeGeneratorFactory(codes []int) ResponseCodeGenerator {
 func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
-	responseBodyGenerator := NewRandomResponseBodyGenerator(10, 10000)
+	responseBodyGenerator := NewRandomResponseBodyGenerator(*minSize, *maxSize)
 	random := NewRealRandom()
 	snoozer := NewRealSnoozer(time.Duration(*minSleep)*time.Millisecond, time.Duration(*maxSleep)*time.Millisecond)
 	handleFactory := NewDefaultEnanosHttpHandlerFactory(responseBodyGenerator, responseCodeGeneratorFactory, snoozer, random)
