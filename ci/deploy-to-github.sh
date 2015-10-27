c4e016706c9a585d2fe9badcf3a4f348d135a03e
Andrew Rea <email@andrewrea.co.uk>
2015-10-27 08:14:26 +0000

    Changing the username

--- ci/deploy-to-github.sh
+++ ci/deploy-to-github.sh
@@ -3,17 +3,17 @@ echo "Travis Branch = $TRAVIS_BRANCH"
 if [ "$TRAVIS_BRANCH" == "master" ]
 then
     gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-    ghr --username reaandrew -c master --token $GITHUB_TOKEN --replace `git describe --abbrev=0 --tags` dist/  
+    ghr -u guzzlerio -r enanos -c master --token $GITHUB_TOKEN --replace `git describe --abbrev=0 --tags` dist/  
 fi
 
 if [ "$TRAVIS_BRANCH" == "release" ]
 then
     gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-    ghr --username reaandrew -c release --token $GITHUB_TOKEN --delete --replace --prerelease pre-release dist/  
+    ghr -u guzzlerio -r enanos -c release --token $GITHUB_TOKEN --delete --replace --prerelease pre-release dist/  
 fi
 
 if [ "$TRAVIS_BRANCH" == "develop" ]
 then
     gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-    ghr --username reaandrew -c develop --token $GITHUB_TOKEN --delete --replace --prerelease latest dist/  
+    ghr -u guzzlerio -r enanos -c develop --token $GITHUB_TOKEN --delete --replace --prerelease latest dist/  
 fi
